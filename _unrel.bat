b35f8b9ba99a53ee5ce09afb7f5d989e9dc468d0
Jace <jaceorwell@gmail.com>
2022-04-03 11:33:38 +0800

    update script for unreleased docs.

--- _unrel.bat
+++ _unrel.bat
@@ -1,4 +1,4 @@
 
 set /p Tag=<doc/last_tag.txt
 echo Last Tag is %Tag%
-git --no-pager log %Tag%..HEAD --pretty=format:"+ %%s (%%aN)" > doc/unreleased.md --author="Jace"
+git --no-pager log %Tag%..HEAD --pretty=format:"+ %%s" > doc/unreleased.md --author="Jace"
