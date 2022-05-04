0201b16cb596898094e2244f5d83acc5b7011691
Jace <jaceorwell@gmail.com>
2022-05-04 12:11:39 +0800

    [skip ci] Skip commits that starts with [skip ci]

--- _unrel.bat
+++ _unrel.bat
@@ -1,4 +1,4 @@
 
 set /p Tag=<doc/last_tag.txt
 echo Last Tag is %Tag%
-git --no-pager log %Tag%..HEAD --pretty=format:"+ %%s" > doc/unreleased.md --author="Jace"
+git --no-pager log %Tag%..HEAD --author="Jace" --invert-grep --grep="^\[skip ci" --pretty=format:"+ %%s" > doc/unreleased.md
