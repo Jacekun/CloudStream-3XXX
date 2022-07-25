c3ae4346c3fdd74719e618cc522c691481a33dab
Jace <jaceorwell@gmail.com>
2022-07-25 17:13:35 +0800

    [skip ci] Better script for unreleased documentation.

--- _unrel.bat
+++ _unrel.bat
@@ -1,4 +1,4 @@
 
 set /p Tag=<doc/last_tag.txt
 echo Last Tag is %Tag%
-git --no-pager log %Tag%..HEAD --author="Jace" --invert-grep --grep="^\[skip ci" --pretty=format:"+ %%s" > doc/unreleased.md
+git log %Tag%..HEAD ^^upstream/master --oneline --invert-grep --grep="^\[skip ci\|^\Merge branch" --pretty=format:"+ %%s" > doc/unreleased.md
